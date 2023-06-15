c5640b00212b74467ce2c5b466cbbc8c83dce832
Normanjpg <normanjpg@gmail.com>
2023-06-15 18:21:57 +0700

    update: perbaiki conflict

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./App.css";
-// import LandingPageJumbotron from "./pages/Landing Pages/Jumbotron/LandingPageJumbotron";
-// import DownloadApp from "./pages/Landing Pages/DownloadApp/DownloadApp";
-// import FooterLanding from "./components/FooterLanding/FooterLanding";
-// import HeaderLanding from "./components/Header/HeadearLanding";
-// import CardHeader from "./components/Header/CardHeader";
-// import MiniCardLanding from "./components/MiniCardLanding/MiniCardLanding";
-// import bg from "./assets/icons/Category.svg";
-// import TestimoniCarousel from "./pages/Landing Pages/TestimoniCarousel/TestimoniCarousel";
-// import Section3LandingPage from "./pages/Landing Pages/Section3LandingPage/Section3LandingPage";
-import MainsLanding from "./pages/Landing Pages/MainsLanding";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FeedbackPages from "./pages/Feedback Pages/FeedbackPages";
-
+import MainLanding from "./pages/Landing Pages/mainLanding";
 function App() {
-  return (
-    <Router>
-      <div
-        className="mainContainer"
-        style={{ backgroundColor: "var(--Neutral-White-100)" }}
-      >
-        <Routes>
-          <Route path="/" exact element={<MainsLanding />} />
-          <Route path="/support" exact element={<FeedbackPages />} />
-          <Route />
-          <Route />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <MainLanding />;
 }
 
 export default App;
---
+++ src/pages/Landing Pages/mainLanding.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import HeaderLanding from "../../components/Header/HeadearLanding";
+import LandingPageJumbotron from "./Jumbotron/LandingPageJumbotron";
+import DownloadApp from "./DownloadApp/DownloadApp";
+import FooterLanding from "../../components/FooterLanding/FooterLanding";
+import CardHeader from "../../components/Header/CardHeader";
+import MiniCardLanding from "../../components/MiniCardLanding/MiniCardLanding";
+import bg from "../../assets/icons/Category.svg";
+import TestimoniCarousel from "./TestimoniCarousel/TestimoniCarousel";
+import Section3LandingPage from "./Section3LandingPage/Section3LandingPage";
+import { Link, animateScroll as scroll } from "react-scroll";
+const MainLanding = () => {
+  return (
+    <div style={{ backgroundColor: "var(--Neutral-White-100)" }}>
+      <HeaderLanding
+        logoButton={
+          <>
+            <Link
+              to="/"
+              className="btn d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+              onClick={() => scroll.scrollToTop()}>
+              <span
+                className="fs-4 fw-bolder"
+                style={{ color: "var(--primary-600)" }}>
+                GoFit
+              </span>
+            </Link>
+          </>
+        }
+      />
+      <LandingPageJumbotron />
+      <Section3LandingPage />
+      <div className="mb-5 mt-5">
+        <TestimoniCarousel />
+      </div>
+      <div className="mb-5 mt-5 container">
+        <MiniCardLanding />
+      </div>
+      <DownloadApp />
+      <div style={{ marginTop: "20vh" }}>
+        <FooterLanding />
+      </div>
+    </div>
+  );
+};
+
+export default MainLanding;
