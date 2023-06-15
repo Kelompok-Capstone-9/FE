97a3615756c3246ed8e10e5bd9931f8f49ab7815
neizshya <nurul.adzan24@gmail.com>
2023-06-15 17:48:15 +0700

    change : logo,responsive header,minicard add : scroll to top button

--- src/App.js
+++ src/App.js
@@ -12,17 +12,32 @@ import MiniCardLanding from "./components/MiniCardLanding/MiniCardLanding";
 import bg from "./assets/icons/Category.svg";
 import TestimoniCarousel from "./pages/Landing Pages/TestimoniCarousel/TestimoniCarousel";
 import Section3LandingPage from "./pages/Landing Pages/Section3LandingPage/Section3LandingPage";
-
+import { Link, animateScroll as scroll } from "react-scroll";
 function App() {
   return (
     <div style={{ backgroundColor: "var(--Neutral-White-100)" }}>
-      <HeaderLanding />
+      <HeaderLanding
+        logoButton={
+          <>
+            <Link
+              to="/"
+              className="btn d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+              onClick={() => scroll.scrollToTop()}>
+              <span
+                className="fs-4 fw-bolder"
+                style={{ color: "var(--primary-600)" }}>
+                GoFit
+              </span>
+            </Link>
+          </>
+        }
+      />
       <LandingPageJumbotron />
       <Section3LandingPage />
       <div className="mb-5 mt-5">
         <TestimoniCarousel />
       </div>
-      <div className="mb-5 mt-5">
+      <div className="mb-5 mt-5 container">
         <MiniCardLanding />
       </div>
       <DownloadApp />
--- src/components/MiniCardLanding/MiniCardLanding.jsx
+++ src/components/MiniCardLanding/MiniCardLanding.jsx
@@ -3,16 +3,18 @@ import "./MiniCardLanding.css";
 const MiniCardLanding = () => {
   return (
     <>
-      <div
-        className="card text-center container miniCardLanding rounded-4 shadow"
-        style={{ width: "25vw" }}>
+      <div className="card text-center container miniCardLanding rounded-4 shadow col-lg-6">
         <div className="card-body">
           <p className="card-title miniCardLandingTitle fs-4 fw-bold">
             Haven't found an answer to your question?
           </p>
-          <p className="card-text miniCardLandingBody fw-medium">
-            Connect with us either on WhatsApp or email us
-          </p>
+
+          <div className="d-none d-lg-flex col-12 card-text miniCardLandingBody fw-medium  align-items-center justify-content-center fs-5">
+            <p>Connect with us either on WhatsApp or email us</p>
+          </div>
+          <div className="d-lg d-lg-none col-12 card-text miniCardLandingBody fw-medium d-flex align-items-center justify-content-center">
+            <p>Connect with us either on WhatsApp or email us</p>
+          </div>
           <div className="row">
             <div className="col-6">
               <button className="btnMiniCard1">Ask on WhatsApp</button>
